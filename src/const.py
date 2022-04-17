e1e8368fa7601c63f6c9fff75150eb5b74745e39
tmxkqotnl <tmxkqotnl@gmail.com>
2022-04-17 14:59:17 +0900

    feat, working

--- src/const.py
+++ src/const.py
@@ -2,3 +2,7 @@ from os.path import dirname,abspath
 
 data_dir_path = dirname(abspath(__file__))
 dependency = ['tensorflow','pandas','matplotlib','seaborn','scikit-learn','ipykernel']
+
+KB = 1024
+MB = 1024 * 1024
+GB = 1024 * 1024 * 1024
